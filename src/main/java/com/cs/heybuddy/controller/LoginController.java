da1b750089fd79aa2d20b12f526dc3b77248622d
priyaranjankumar_cs <priyaranjan.kumar@contentserv.com>
2021-04-12 16:26:03 +0530

    adding login controller

---
+++ src/main/java/com/cs/heybuddy/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.cs.heybuddy.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+	@GetMapping("/login")
+	public ResponseEntity<Double> login() {
+		 return ResponseEntity.status(HttpStatus.OK)
+					.body(Math.random());
+
+	}
+}
