71987581fbe78ba12c6dc6f7cf51529dfce7719b
priyaranjankumar_cs <priyaranjan.kumar@contentserv.com>
2021-04-13 12:47:14 +0530

    Aapplied antmatcher for static resources

--- src/main/java/com/cs/heybuddy/config/WebSecurityConfig.java
+++ src/main/java/com/cs/heybuddy/config/WebSecurityConfig.java
@@ -58,6 +58,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
+			 .antMatchers("/heybuddy/**").permitAll()
+			 .antMatchers("/heybuddy/static/**").permitAll()
+			 .antMatchers("/static/**").permitAll()
+			 .antMatchers("/index.html").permitAll()
+			 .antMatchers("/").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
